bef4e10a26b86ba83377b057e7ebdb541b6771f6
bilalyavas <b.yavas711@gmail.com>
2023-05-05 00:32:57 -0400

    UI5

--- src/test/java/com/omdb/runners/CukesRunner.java
+++ src/test/java/com/omdb/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/omdb/stepsDef",
         dryRun = false,
-        tags = "@odbc"
+        tags = "@Smoke"
       // tags = "@API_Omdb"
 )
 public class CukesRunner {
